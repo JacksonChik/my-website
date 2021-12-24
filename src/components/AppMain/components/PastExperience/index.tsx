03926cec5e82bff2f34873fc478cf81271a1f979
Chik Jackson <Jacksonchik01@hotmail.com>
2021-12-24 06:27:58 -0800

    restyled past experience

--- src/components/AppMain/components/PastExperience/index.tsx
+++ src/components/AppMain/components/PastExperience/index.tsx
@@ -4,19 +4,38 @@ import "./style.scss"
 const PastExperience = () =>{
     return(
         <div>
-            <div className="past-experience">
-                <header>
-                    Full stack developer @ KM.ON by Karl Mayer
+            <div className="work-experience">
+                <header className="section-title">
+                    Work experience
                 </header>
-                <p>
-                    I worked here for 3 months(2021.06 - 2021.09) as an intern. Absolutely amazing workplace culture, the tech stack's
-                    also cool as hell(My team used TypeScript w/ React.js + Scala w/ Play + Postgresql). Learnt a lot from my mentors
-                    and made some good friends.
-                </p>
-                <p>
-                    My other offer at that time was to work as an embedded SWE intern for ASM Pacific Technology LTD. But I don't regret
-                    it one bit for choosing KM.ON.
-                </p>
+                <header className="work-experience--job-title">
+                Full Stack Developer @ KM.ON by Karl Mayer Hong Kong (2021.06 – 2021.09)
+                </header>
+                <ul className="work-experience--job-duties">
+                    <li>
+                    Implemented performance critical RESTful APIs and deployed endpoints for enterprise level web applications with Scala Play.
+                    </li>
+                    <li>
+                    Queries and access with Scala’s Slick library to PostgreSQL database. Implemented CSV validation on the backend and ensured customer data integrity.
+                    </li>
+                    <li>
+                    Created a highly scalable boilerplate application with React.js under supervision, which is now used by KM.ON’s engineering teams in Hong Kong, Germany, and India.
+                    </li>
+                    <li>
+                    Frontend UI/UX design with Figma. Implemented new reusable components to the company’s UI library with React.js, which are now used in all company projects.
+                    </li>
+                    <li>
+                    Implemented extensive unit tests and test suites on the backend with ScalaTest. A 90%+ test coverage was achieved.
+                    </li>
+                    <li>
+                    Peer review, agile process. Jira release note automated generation & delivery.
+                    </li>
+                </ul>
+            </div>
+            <div className="relevant-coursework">
+                <header className="section-title">
+                    Relevant coursework
+                </header>
             </div>
         </div>
     )
